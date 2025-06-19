fba1acd5fa34ba4610db43c86ef7b3016152dc0b
sahil <sahil@creditoracademy.com>
2025-06-19 19:52:43 +0530

    added dyanamic properties

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,9 +7,9 @@ const app = express();
 
 // Allowed Origins for CORS
 const allowedOrigins = [
-  "http://127.0.0.1:5500",
-  "http://localhost:5500",
-  "http://127.0.0.1:5500/docs" ,
+  "http://127.0.0.1:5503",
+  "http://localhost:5503",
+  "http://127.0.0.1:5503/docs" ,
   "https://prernamishra29.github.io/Illuminant/index.html"
   // Optional, include if you're serving from here
 ];
